7a9a4cbdd2520ff37e52bd009fe5481bbda6b257
wakuma-dev <wakuma.hailu.dev@gmail.com>
2026-05-26 18:25:39 +0300

    fix:(deployment) gh-pages problem

--- my-project/src/main.jsx
+++ my-project/src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/shevan'>
       <App />
     </BrowserRouter>
   </StrictMode>,
