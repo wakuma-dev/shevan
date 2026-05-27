ed5c75b85253abb84d02b9a72cf36564741eb7c8
wakuma-dev <wakuma.hailu.dev@gmail.com>
2026-05-27 06:43:59 +0300

    Updates

--- assets/ContactUs-BiuqLugg.js
+++
@@ -1,1 +0,0 @@
-import{i as e,n as t}from"./index-BnKdkDr9.js";e();var n=t(),r=()=>(0,n.jsx)(`div`,{});export{r as default};
---
+++ assets/ContactUs-CDiqmrCd.js
@@ -0,0 +1,1 @@
+import{a as e,c as t}from"./index-CPCy4AvG.js";t();var n=e(),r=()=>(0,n.jsx)(`div`,{});export{r as default};
